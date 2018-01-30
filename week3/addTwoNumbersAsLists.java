3d3bc5db3ded7aa47d61699ccaf398203c1ea8bd
Kevin Farst <kevin.farst@zozi.com>
2018-01-29 21:26:25 -0800

    Add more week 3 solutions

---
+++ week3/addTwoNumbersAsLists.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode A, ListNode B) {
+        ListNode res = null;
+        ListNode prev = null;
+        ListNode temp = null;
+        int carry = 0, sum;
+
+        while (A != null || B != null) {
+            sum = carry + (A != null ? A.val : 0) + (B != null ? B.val : 0);
+
+            carry = (sum >= 10) ? 1 : 0;
+
+            sum = sum % 10;
+
+            temp = new ListNode(sum);
+
+            if (res == null) {
+                res = temp;
+            } else {
+                prev.next = temp;
+            }
+
+            prev = temp;
+
+            if (A != null) {
+                A = A.next;
+            }
+
+            if (B != null) {
+                B = B.next;
+            }
+        }
+
+        if (carry > 0) {
+            temp.next = new ListNode(carry);
+        }
+
+        return res;
+    }
+}
